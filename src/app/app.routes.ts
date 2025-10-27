37b5432c0cce5c7b8f23cfadcea979de3bf5df4a
robinPecheurBstorm <robin.pecheur@bstorm.be>
2025-10-27 14:09:23 +0100

    Projet Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Demo1 } from './features/demo/pages/demo1/demo1';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component: Home,
+        title: 'ma homepage'
+    },
+    {
+        path:'lazyHome',
+        loadComponent:()=>import('./features/home/home').then(m => m.Home),
+        title: 'ma homepage en lazymode'
+    },
+    // Les demos
+    {
+        path: 'demo/demo1',
+        component: Demo1,
+        title: 'Mon premier component'
+    },
+];
