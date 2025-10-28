0c42092921430ef592d01bc267e721cb707a0032
robinPecheurBstorm <robin.pecheur@bstorm.be>
2025-10-28 14:30:05 +0100

    Correction exercice sur les directives

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Link } from '../../models/link';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,25 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  links: Link[] = [
+    new Link("Home","/home",undefined,true),
+    new Link("Mes demos",undefined,[
+      new Link("Mon premier component","/demo/demo1",undefined,false),
+      new Link("Les bindings","/demo/demo2",undefined,false),
+      new Link("Le pipes","/demo/demo3",undefined,false),
+      new Link("Le directives","/demo/demo4",undefined,false),
+    ],true),
+    new Link("Mes exercices",undefined,[
+      new Link("Le chronomètre","/exo/exo1",undefined,false),
+      new Link("La navbar","/exo/exo2",undefined,false),
+    ],true),
+  ];
 
+  displayHideChildLink(index: number){
+    if (this.links[index] && this.links[index].children) {
+      for (let child of this.links[index].children){
+        child.isVisible = !child.isVisible;
+      }
+    }
+  }
 }
